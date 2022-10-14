afedc0663a6c31fc3c80e273d6a612ad2d738cb1
loading-content <56720934+loading-content@users.noreply.github.com>
2022-10-14 10:10:39 +0200

    Add files via upload

---
+++ cuperter spel/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wat is het beste cumputer spel?");
+            string cum = Console.ReadLine();
+            Console.WriteLine(cum);
+        }
+    }
+}
