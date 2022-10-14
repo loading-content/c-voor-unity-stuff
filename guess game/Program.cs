c8c55f8a99bd743eface31d35bf9a3359355b3a0
loading-content <56720934+loading-content@users.noreply.github.com>
2022-10-14 10:11:19 +0200

    Add files via upload

---
+++ guess game/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 3)
+            {
+                int v = 3 - i;
+                int guesses = v;
+                Console.WriteLine("Its a bird. ");
+                Console.WriteLine("Guess something. ");
+                Console.WriteLine("you have " + guesses + " guesses left. ");
+                string a = Console.ReadLine();
+                if (a == "duck")
+                {
+                    Console.WriteLine("Correct! ");
+                    i = 3;
+                }
+                i++;
+            }
+            if (i == 3)
+            {
+                Console.WriteLine("You've failed :( ");
+            }
+        }
+    }
+}
